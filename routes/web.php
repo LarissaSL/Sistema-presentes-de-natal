6f0a1e41b8cbe4a54c94efd96c226a6c5b73eeed
LarissaSL <larissa.silvaedge@gmail.com>
2025-01-01 20:42:36 -0300

    refactor: arrumando as rotas

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::get('/login', 'login')->name('auth.login');
-    Route::POST('/login', 'loginSubmit')->name('auth.loginSubmit');
+    Route::post('/login', 'loginSubmit')->name('auth.loginSubmit');
     Route::get('/logout', 'logout')->name('auth.logout')->middleware(CheckUserLogged::class);
 });
 
@@ -21,27 +21,23 @@ Route::controller(MainController::class)->group(function () {
 
 Route::controller(UserController::class)->group(function() {
     Route::get('/register', 'register')->name('user.register');
-    Route::POST('/register', 'registerSubmit')->name('user.registerSubmit');
+    Route::post('/register', 'registerSubmit')->name('user.registerSubmit');
 
-    Route::prefix('/user')->group(function (){
-        Route::middleware([CheckUserLogged::class, CheckUser::class])->group(function() {
-            Route::get('/dashboard', 'index')->name('user.dashboard')->withoutMiddleware(CheckUser::class);
-            Route::get('/my-profile', 'read')->name('user.myProfile');
-            Route::POST('/my-profile/update', 'update')->name('user.update');
-            Route::get('/my-profile/delete', 'delete')->name('user.delete');
-        });
+    Route::prefix('/user')->middleware([CheckUserLogged::class, CheckUser::class])->group(function () {
+        Route::get('/dashboard', 'index')->name('user.dashboard');
+        Route::get('/my-profile', 'read')->name('user.myProfile');
+        Route::post('/my-profile/update', 'update')->name('user.update');
+        Route::get('/my-profile/delete', 'delete')->name('user.delete');
     });
 });
 
 Route::controller(ContactController::class)->group(function() {
-    Route::prefix('/user/contact')->group(function (){
-        Route::middleware([CheckUserLogged::class, CheckUser::class])->group(function() {
-            Route::get('/', 'index')->name('contact.listContacts');
-            Route::get('/create', 'create')->name('contact.create')->withoutMiddleware([CheckUser::class]);
-            Route::POST('/createSubmit', 'createSubmit')->name('contact.createSubmit');
-            Route::get('/updtate/{id}', 'update')->name('contact.update')->withoutMiddleware([CheckUser::class])->middleware(CheckContact::class);
-            Route::POST('/updateSubmit', 'updateSubmit')->name('contact.updateSubmit');
-            Route::get('/delete/{id}', 'delete')->name('contact.delete');
-        });
+    Route::prefix('/user/contact')->middleware([CheckUserLogged::class, CheckUser::class])->group(function () {
+        Route::get('/', 'index')->name('contact.listContacts');
+        Route::get('/create', 'create')->name('contact.create');
+        Route::post('/createSubmit', 'createSubmit')->name('contact.createSubmit');
+        Route::get('/update/{id}', 'update')->name('contact.update')->middleware(CheckContact::class);
+        Route::post('/updateSubmit', 'updateSubmit')->name('contact.updateSubmit');
+        Route::get('/delete/{id}', 'delete')->name('contact.delete');
     });
 });
